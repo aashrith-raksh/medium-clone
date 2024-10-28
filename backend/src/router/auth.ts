7f516c1c21565efa9a64846ce476a43ad5181c8d
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-10-28 17:17:12 +0530

    feat: Add signup and signin

--- backend/src/router/auth.ts
+++ backend/src/router/auth.ts
@@ -2,23 +2,94 @@ import { Hono } from "hono";
 
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign, verify } from "hono/jwt";
 
 const authRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>();
 
-authRouter.post("/signup", (c) => {
+authRouter.post("/signup", async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
 
-  return c.text("SIGNUP HIT");
+  try {
+    const { email, password } = await c.req.json();
+
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        password,
+      },
+    });
+
+    const token = await sign(
+      {
+        id: newUser.id,
+      },
+      c.env.JWT_SECRET
+    );
+
+    return c.json(
+      {
+        message: "Sign up successful",
+        token,
+      },
+      200
+    );
+  } catch (error) {
+    if (error instanceof Error) console.log(error.message);
+
+    return c.json(
+      {
+        //@ts-ignore
+        message: error.message || "Error signing up",
+      },
+      500
+    );
+  }
 });
 
-authRouter.post("/signin", (c) => {
-  return c.text("SIGNIN HIT");
+authRouter.post("/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const { email, password } = await c.req.json();
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+        password
+      },
+    });
+
+    if (!user) {
+      return c.json(
+        {
+          message: "Invalid credentials",
+        },
+        403
+      );
+    }
+
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json(
+      {
+        message: "Sign In successful",
+        token: jwt
+      },
+      200
+    );
+  } catch (error) {
+    return c.json({
+      // @ts-ignore
+      message: error.message || "error while sign in ",
+    });
+  }
 });
 
 export default authRouter;
